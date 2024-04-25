3536ca8fab31fe4707a8d1a62b08d9feee332618
Rincla <7842@holbertonstudents.com>
2024-04-25 22:20:32 +0200

    fix project 3

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-""" async comprehension """
-import typing
+""" Async comprenhesion gen """
+import asyncio
+from typing import List
+
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List[float]:
-    """ async comprehension """
-    result = [v async for v in async_generator()]
-    result = [float(v) for v in result]
-    return result
+async def async_comprehension() -> List[float]:
+    """
+        Generate numbers with async comprenhension
+
+        Args:
+            void
+
+        Return:
+            float random numbers
+    """
+    return ([i async for i in async_generator()])
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" measure runtime """
+""" Measure times """
 import asyncio
 import time
 
@@ -7,10 +7,18 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ measure runtime """
-    s = time.time()
-    fn = async_comprehension
-    lst = [fn() for _ in range(4)]
-    await asyncio.gather(*lst)
-    e = time.time()
-    return e - s
+    """
+        measure time and execute in paralallel
+
+        Args:
+            void
+
+        Return:
+            float random numbers
+    """
+    first_time = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = time.perf_counter()
+
+    return (elapsed - first_time)
